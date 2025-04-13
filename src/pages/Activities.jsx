2cea581690ac85cc18a99f477c9536b3168f24ac
ArfiTech <arfitech0425@gmail.com>
2025-04-13 16:00:23 +0000

    modify activities

--- src/pages/Activities.jsx
+++ src/pages/Activities.jsx
@@ -4,7 +4,7 @@ function Activities() {
         <h2 className="text-xl font-bold mb-2">Activities</h2>
         <ul className="list-disc list-inside">
           <li>2020.03 ~ 2024.02 - CAU B.S in CSE</li>
-          <li>2025.01 ~ present - Ngine Studios AI developer</li>
+          <li>2024.01 ~ present - Ngine Studios AI developer</li>
         </ul>
       </section>
     );
